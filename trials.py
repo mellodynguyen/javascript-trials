735e89ccef4ba0756903cecea2d8eadf5968901e
Mellody <mellodynguyen@outlook.com>
2023-12-18 15:03:49 -0600

    did most functions

--- trials.py
+++ trials.py
@@ -2,27 +2,62 @@
 
 
 def output_all_items(items):
-    pass  # TODO: replace this line with your code
+    for item in items:
+        print(item)
+    return None
 
 
 def get_all_evens(nums):
-    pass  # TODO: replace this line with your code
-
+    even_nums = []
+    for num in nums:
+        if num % 2 == 0:
+            even_nums.append(num)
+    return even_nums
 
 def get_odd_indices(items):
-    pass  # TODO: replace this line with your code
-
+    odd_nums = []
+    nums = items
+    for num in nums:
+        if num % 2 != 0:
+            odd_nums.append(num)
+    return odd_nums
 
 def print_as_numbered_list(items):
-    pass  # TODO: replace this line with your code
+    for i, item in enumerate(items):
+            print(f"{i}, {item}")
+    return None
 
 
 def get_range(start, stop):
-    pass  # TODO: replace this line with your code
+    nums = []
+    # i = 0
+    # while i < 10:
+    #     i + 1 
+    #     print (i)
 
+    for num in range(start, stop):
+        nums.append(num)
+    # return nums
+    print(nums)
+# input 0 - 10 (start, stop)
+# expected output
+# 0, 1, 2, 3, 4, 5, 6, 7, ... 
+# 10 - 20
+# 10, 11, 12 ... 
 
 def censor_vowels(word):
-    pass  # TODO: replace this line with your code
+    censored_word = []
+    vowel = 'aeiou'
+    
+    for letter in word:
+        if letter in vowel:
+            censored_word.append('*')
+        else:
+            censored_word.append(letter)
+    
+    return ''.join(censored_word)
+    # return word
+print(censor_vowels("dog"))
 
 
 def snake_to_camel(string):
